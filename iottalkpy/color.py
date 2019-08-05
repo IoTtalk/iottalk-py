b3e1c6a9216a20bff9bb7c184c96acab31dc8f3e
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-08-05 16:12:30 +0800

    no Windows color support

--- iottalkpy/color.py
+++ iottalkpy/color.py
@@ -1,3 +1,6 @@
+import platform
+
+
 class ColorBase:
     default = "\033[0m"
     data = "\033[1;33m"
@@ -7,6 +10,13 @@ class ColorBase:
         """
         wrap string with color
         """
+        if platform.system() == 'Windows':
+            # If we want to support colored printing on Windows.
+            # The easiest way is https://github.com/tartley/colorama,
+            # but this introduces extra dependency.
+            # The implementation for Windows is not small enought to self-shipped.
+            return s
+
         return "{}{}{}".format(color, s, cls.default)
 
 
